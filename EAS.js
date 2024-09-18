096116fd649ca3e74eb4dfcedcc4a796e456e790
Kiefer Hillerich <beacheddogcreations@gmail.com>
2024-09-18 14:42:30 -0400

    updated to add reset button and color changing effects

--- EAS.js
+++ EAS.js
@@ -1,17 +1,25 @@
 const container = document.querySelector(".container")
+const btn = document.querySelector(".btn")
+let number = 16
 
 
 function randomColor(){
     const r = Math.floor(Math.random()*255);
     const g = Math.floor(Math.random()*255);
     const b = Math.floor(Math.random()*255);
-    return `rgb(${r}, ${g}, ${b})`;
+    return { r, g, b };
 }
 
+function darkenColor(rgb, factor){
+    const r = Math.floor(rgb.r * (1-factor));
+    const g = Math.floor(rgb.g * (1-factor));
+    const b = Math.floor(rgb.b * (1-factor));
+   return `rgb(${r}, ${g}, ${b})`;
+}
 
 function board(size){
-
-    let squareSize= 960/size
+    container.innerHTML=""
+    let squareSize= 350/size
 
     for (let i=0; i<size*size; i++){
         const cell = document.createElement("div");
@@ -19,16 +27,26 @@ function board(size){
         cell.style.width= `${squareSize}px`;
         cell.style.height= `${squareSize}px`;
         let color = randomColor()
+        let darken = 0
 
-        cell.addEventListener("mouseenter", () => {
-            cell.style.backgroundColor = color
-        })
-
-
-
+        cell.addEventListener('mouseenter', () => {
+            if (darken < 1) {
+                darken += 0.1;
+                cell.style.backgroundColor = darkenColor(color, darken);
+            }
+        });
         container.appendChild(cell);
     }
 }   
+    btn.addEventListener("click", () => {
+        let newSize = parseInt(prompt('Enter new grid size (max 100):'));
+        if (newSize > 100) newSize = 100;
+        if (newSize && newSize > 0) {
+        number = newSize;
+        board(number);
+}
+    })
 
-board(5)
+
+board(number)
 
